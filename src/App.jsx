ef29e9781a57ec63dca78634354136d812203b04
wljuancho <quinteroroldanjuanangel@gmail.com>
2026-08-05 15:02:47 -0500

    m

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+
+export default function App() {
+  // Esta es nuestra "base de datos" inicial quemada en el código
+  const contactos = [
+    {
+      id: 1,
+      nombre: "Carolina Pérez",
+      telefono: "300 123 4567",
+      correo: "carolina@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+    {
+      id: 2,
+      nombre: "Juan Díaz",
+      telefono: "301 987 6543",
+      correo: "juan@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 3,
+      nombre: "Luisa Martínez",
+      telefono: "320 555 7788",
+      correo: "luisa@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+  ];
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+
+      {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */}
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id}            // key única para React
+          nombre={c.nombre}     // prop nombre
+          telefono={c.telefono} // prop telefono
+          correo={c.correo}     // prop correo
+          etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.)
+        />
+      ))}
+
+      <p className="app-nota">
+        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
+      </p>
+    </main>
+  );
+}
+
---
+++ src/components/ContactoCard.jsx
@@ -0,0 +1,19 @@
+export default function ContactoCard({  nombre, telefono, correo, etiqueta }) {
+  return (
+    <div className="card-contacto">
+      {/* Nombre del contacto en negrita */}
+      <h3 className="card-nombre">{nombre}</h3>
+
+      {/* Teléfono */}
+      <p className="card-linea">📞 {telefono}</p>
+
+      {/* Correo */}
+      <p className="card-linea">📧 {correo}</p>
+
+      {/* Etiqueta adicional, si existe (por ejemplo: "Instructor", "Cliente", "Compañera") */}
+      {etiqueta && (
+        <p className="card-etiqueta">{etiqueta}</p>
+      )}
+    </div>
+  );
+}
